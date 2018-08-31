370d2d229d2c47c502ca3a5923c2526626a231c1
Chris <christoph_glatthor@web.de>
2018-08-31 17:02:15 +0200

    removed socket io, adding cors for using localhost as host

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const http = require('http'),
-    express = require('express'),
-    bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 
-const socketManager = require('./socket-manager'),
-    login = require('./routes/login-route'),
+const http = require('http').Server(app),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
+
+const login = require('./routes/login-route'),
     messages = require('./routes/messages-route');
 
-const PORT = '4000',
-    HOST = '192.168.2.116'; //IP has to be updated when starting the server
+const PORT = 4000;
 
-var app = express();
 // middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 //routes
 app.use('/login', login);
 app.use('/messages', messages);
 
-var server = http.createServer(app)
-server.listen(PORT, HOST, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Server is running on ' + HOST + ':' + PORT);
-    }
+http.listen(PORT, function () {
+    console.log(`App listening on port ${PORT}!`);
 });
